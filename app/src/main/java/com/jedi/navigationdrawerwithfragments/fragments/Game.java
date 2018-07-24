64279848c6aa7ef722dbf176a2a87df8cfa03c5d
Roger Guasch <30463479+roguib@users.noreply.github.com>
2018-07-24 18:41:50 +0200

    Memory card game added

--- app/src/main/java/com/jedi/navigationdrawerwithfragments/fragments/Game.java
+++ app/src/main/java/com/jedi/navigationdrawerwithfragments/fragments/Game.java
@@ -3,29 +3,45 @@ package com.jedi.navigationdrawerwithfragments.fragments;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.media.Image;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Chronometer;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.jedi.navigationdrawerwithfragments.LoginActivity;
 import com.jedi.navigationdrawerwithfragments.R;
 
+import java.util.Random;
+
 /**
  * A simple {@link Fragment} subclass.
  */
 public class Game extends Fragment {
 
+    private Chronometer c;
     Button logOut;
     ImageButton b0, b1, b2, b3;
     ImageButton b4, b5, b6, b7;
     ImageButton b8, b9, b10, b11;
     ImageButton b12, b13, b14, b15;
+    int cards[] = {R.drawable.as, R.drawable.as, R.drawable.five_of_diamonds, R.drawable.five_of_diamonds,
+            R.drawable.five_of_hearts, R.drawable.five_of_hearts, R.drawable.jack_of_diamonds, R.drawable.jack_of_diamonds,
+            R.drawable.nine_of_trebols, R.drawable.nine_of_trebols, R.drawable.queen_of_trebols, R.drawable.queen_of_trebols,
+            R.drawable.seven_of_diamonds, R.drawable.seven_of_diamonds, R.drawable.two_of_hearts, R.drawable.two_of_hearts};
+    Boolean firstCardPressed;
+    Boolean stardOfTheGame;
+    ImageButton firstImgButton;
+    int firstCardId;
+    int found;
+    String finalTime;
 
     public Game() {
         // Required empty public constructor
@@ -37,9 +53,9 @@ public class Game extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         super.onCreate(savedInstanceState);
-
         View v = inflater.inflate(R.layout.fragment_game, container, false);
         //textViewResult = (TextView) v.findViewById(R.id.textViewResult);
+        c = (Chronometer) v.findViewById(R.id.chrono);
         b0 = (ImageButton) v.findViewById(R.id.imageButton1);
         b1 = (ImageButton) v.findViewById(R.id.imageButton2);
         b2 = (ImageButton) v.findViewById(R.id.imageButton3);
@@ -55,18 +71,21 @@ public class Game extends Fragment {
         b12 = (ImageButton) v.findViewById(R.id.imageButton13);
         b13 = (ImageButton) v.findViewById(R.id.imageButton14);
         b14 = (ImageButton) v.findViewById(R.id.imageButton15);
+        b15 = (ImageButton) v.findViewById(R.id.imageButton16);
         logOut = (Button) v.findViewById(R.id.logOut);
+        firstCardPressed = false;
+        stardOfTheGame = false;
+        finalTime = "";
+        found = 0;
         //textViewResult = (TextView) v.findViewById(R.id.textViewResult);
-
+        shuffleArray(cards);
         //en vez de int es una variable click
-        View.OnClickListener appendNumber = new View.OnClickListener() {
+
+        View.OnClickListener appendNumber2 = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Button b = (Button) view;
-                /* girar la imagen:
-                b0.setImageResource(R.drawable.as); */
-                if(b == logOut)
-                {
+                if (b == logOut) {
                     SharedPreferences prefs = getActivity().getSharedPreferences("sp", 0);
                     SharedPreferences.Editor editor = prefs.edit();
                     editor.putBoolean("open session", false);
@@ -75,6 +94,74 @@ public class Game extends Fragment {
                     Intent intent = new Intent(getActivity(), LoginActivity.class);
                     intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                     startActivity(intent);
+                }
+            }
+        };
+
+        View.OnClickListener appendNumber = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ImageButton ib = (ImageButton) view;
+                /* girar la imagen:
+                b0.setImageResource(R.drawable.as); */
+                if(ib == b0) {
+                    changeImageButton(ib, 0);
+                }
+                else if(ib == b1) {
+                    changeImageButton(ib, 1);
+                }
+                else if(ib == b2) {
+                    changeImageButton(ib, 2);
+                }
+                else if(ib == b3) {
+                    changeImageButton(ib, 3);
+                }
+                else if(ib == b4) {
+                    changeImageButton(ib, 4);
+                }
+                else if(ib == b5) {
+                    changeImageButton(ib, 5);
+                }
+                else if(ib == b6) {
+                    changeImageButton(ib, 6);
+                }
+                else if(ib == b7) {
+                    changeImageButton(ib, 7);
+                }
+                else if(ib == b8) {
+                    changeImageButton(ib, 8);
+                }
+                else if(ib == b9) {
+                    changeImageButton(ib, 9);
+                }
+                else if(ib == b10) {
+                    changeImageButton(ib, 10);
+                }
+                else if(ib == b11) {
+                    changeImageButton(ib, 11);
+                }
+                else if(ib == b12) {
+                    changeImageButton(ib, 12);
+                }
+                else if(ib == b13) {
+                    changeImageButton(ib, 13);
+                }
+                else if(ib == b14) {
+                    changeImageButton(ib, 14);
+                }
+                else if(ib == b15) {
+                    changeImageButton(ib, 15);
+                }
+
+
+                if(!stardOfTheGame) {
+                    stardOfTheGame = true;
+                    c.start();
+                }
+                if(found == 8) {
+                    c.stop();
+                    finalTime = c.getText().toString();
+                    Log.v("time", finalTime);
                 }
             }
         };
@@ -94,8 +181,50 @@ public class Game extends Fragment {
         b12.setOnClickListener(appendNumber);
         b13.setOnClickListener(appendNumber);
         b14.setOnClickListener(appendNumber);
-        logOut.setOnClickListener(appendNumber);
+        b15.setOnClickListener(appendNumber);
+        logOut.setOnClickListener(appendNumber2);
         return v;
     }
 
+    public static void shuffleArray(int[] a) {
+        int n = a.length;
+        Random random = new Random();
+        random.nextInt();
+        for (int i = 0; i < n; i++) {
+            int change = i + random.nextInt(n - i);
+            swap(a, i, change);
+        }
+    }
+
+    private static void swap(int[] a, int i, int change) {
+        int helper = a[i];
+        a[i] = a[change];
+        a[change] = helper;
+    }
+
+    private void changeImageButton(final ImageButton ib, int id) {
+        if(!firstCardPressed)
+        {
+            firstCardPressed = true;
+            firstImgButton = ib;
+            ib.setImageResource(cards[id]);
+            firstCardId = cards[id];
+        }
+        else if(firstCardPressed && firstImgButton != ib)
+        {
+            firstCardPressed = false;
+            ib.setImageResource(cards[id]);
+            if(firstCardId == cards[id]) ++found;
+            else
+            {
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        ib.setImageResource(R.drawable.card_back);
+                        firstImgButton.setImageResource(R.drawable.card_back);
+                    }
+                }, 1000);
+            }
+        }
+    }
 }
